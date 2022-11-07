24140007d5d6cc51d7bd42ac1f531b3321b1b39e
NikhilSuresh <nikhil@optalitix.com>
2022-11-07 15:42:56 +0000

    HTML and CSS introduced into the project and created a index.html file.

---
+++ Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace DutchTreat
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+
+            app.UseHttpsRedirection();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}
