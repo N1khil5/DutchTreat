79dfb88c18e403134f7aaca98dc80149baea73ad
NikhilSuresh <nikhil@optalitix.com>
2022-11-08 11:55:07 +0000

    Added Javascript and jQuery to the project

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    console.log("Hello Pluralsight");
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You have clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(100);
+    });
+
+
+});
+
