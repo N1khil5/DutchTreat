8271cc659067d9df631cdd553cf9a2a0602d7f21
Nikhil Suresh <nikhil@optalitix.com>
2022-11-25 17:16:07 +0000

    CAlling the API to get angular data on the DutchTreat application.

--- client/src/app/views/productListView.component.ts
+++ client/src/app/views/productListView.component.ts
@@ -1,16 +1,21 @@
-﻿import { Component } from "@angular/core";
+﻿import { Component, OnInit } from "@angular/core";
+import { Store } from "../services/store.service";
 
 @Component({
     selector: "product-list",
     templateUrl: "productListView.component.html"
-    })
+})
 
-export default class ProductListView {
-    public products = [{
-        title: "Van Gogh",
-        price: "19.99"
-    }, {
-        title: "Van Gogh Poster",
-        price: "29.99"
-    }];
+export default class ProductListView implements OnInit{
+    public products: { title: string; price: string; }[];
+
+
+    constructor(public store: Store) {
+        this.products = store.products;
+    }
+
+
+    ngOnInit(): void {
+        this.store.loadProducts().subscribe();
+    }
 }
