2f72d7e651abbae8b90cf4dcfdf832e686c2b040
Nikhil Suresh <nikhil@optalitix.com>
2022-11-28 11:19:31 +0000

    Using services in Angular

--- client/src/app/views/productListView.component.ts
+++ client/src/app/views/productListView.component.ts
@@ -3,19 +3,18 @@ import { Store } from "../services/store.service";
 
 @Component({
     selector: "product-list",
-    templateUrl: "productListView.component.html"
+    templateUrl: "productListView.component.html",
+    styleUrls: ["productsListView.component.css"]
 })
 
 export default class ProductListView implements OnInit{
-    public products: { title: string; price: string; }[];
 
 
     constructor(public store: Store) {
-        this.products = store.products;
     }
-
-
     ngOnInit(): void {
-        this.store.loadProducts().subscribe();
-    }
+        this.store.loadProducts().subscribe(() => {
+
+        });
+        }
 }
